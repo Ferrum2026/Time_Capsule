1c4a5069deca3f74891b487b5ad6d46857da2752
Official <princeofshards108@gmail.com>
2025-11-01 16:39:57 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -2,16 +2,15 @@
 // Create a Firebase project -> Realtime Database -> get your databaseURL
 // Save this file with your actual keys before deploying.
 
- const firebaseConfig = {
-    apiKey: "AIzaSyB9ADMsJMGKO9jvUxpDdclVAHy4RZmileQ",
-    authDomain: "batchcapsule.firebaseapp.com",
-    databaseURL: "https://batchcapsule-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "batchcapsule",
-    storageBucket: "batchcapsule.firebasestorage.app",
-    messagingSenderId: "660821902563",
-    appId: "1:660821902563:web:980ba4ae153fef1ce1f1d6",
+const firebaseConfig = {
+  apiKey: "AIzaSyB9ADMsJMGKO9jvUxpDdclVAHy4RZmileQ",
+  authDomain: "batchcapsule.firebaseapp.com",
+  databaseURL: "https://batchcapsule-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "batchcapsule",
+  storageBucket: "batchcapsule.firebasestorage.app",
+  messagingSenderId: "660821902563",
+  appId: "1:660821902563:web:980ba4ae153fef1ce1f1d6",
 };
 
-// The code below expects firebase to be loaded by the page.
-// We will export it as a global variable used in app.js
-window.__FIREBASE_CONFIG = FIREBASE_PLACEHOLDERS;
+// Make Firebase config globally accessible
+window.__FIREBASE_CONFIG = firebaseConfig;
