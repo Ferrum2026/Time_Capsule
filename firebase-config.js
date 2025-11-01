7a5978653385af3537ec0818200f632e81732c72
Official <princeofshards108@gmail.com>
2025-11-01 17:20:53 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+// Your Firebase Realtime Database connection
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9ADMsJMGKO9jvUxpDdclVAHy4RZmileQ",
+  authDomain: "batchcapsule.firebaseapp.com",
+  databaseURL: "https://batchcapsule-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "batchcapsule",
+  storageBucket: "batchcapsule.firebasestorage.app",
+  messagingSenderId: "660821902563",
+  appId: "1:660821902563:web:980ba4ae153fef1ce1f1d6"
+};
+
+// Make config globally available
+window.__FIREBASE_CONFIG = firebaseConfig;
