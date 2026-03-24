e8fddd169ef7c1ff0320a9577ac03b77e067c6b9
Official <princeofshards108@gmail.com>
2026-03-25 03:45:09 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 const appConfig = {
   // 1) EASIEST TIMER TO CHANGE.
   // Example: '2027-12-31T23:59:59'
-  revealIso: '2023-03-22T23:10:59',
+  revealIso: '2027-04-26T23:07:30',
 
   // 2) Set to true if you want to TEST the open vault immediately.
   forceOpenVault: true,
